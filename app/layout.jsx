0f5a68c9de7f08e0b9bab8bce6abd3fe2cc72eae
Ahmad Affandy <ahmadaffandy008@gmail.com>
2025-04-13 03:46:18 +0700

    new

--- app/layout.jsx
+++ app/layout.jsx
@@ -6,8 +6,14 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang="id">
+      <head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, viewport-fit=cover"
+        />
+      </head>
+      <body className="scroll-smooth antialiased">{children}</body>
     </html>
   );
 }
